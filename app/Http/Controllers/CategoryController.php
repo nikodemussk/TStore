45cd997d0e787e9617a80f2ad4bc51c9a43d50dc
Niko <teddybear_happyboy@yahoo.co.id>
2019-10-19 09:52:54 +0700

    first commit

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+// use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
+
+
+class CategoryController extends Controller
+{
+    public function __construct(){
+        // $this->middleware('admin');
+    }
+
+
+    public function index(){
+        $categories = \App\Category::all();
+        return view('category.index',compact('categories'));
+    }
+
+    public function create(){
+        return view("category.create");
+    }
+
+    public function store(){
+        $data = request()->validate([
+            "name" => ["required","min:5", 'unique:categories'],
+            "image" => ['mimes:jpeg,jpg,png','nullable','image']
+        ]);
+
+        if (request('image')) {
+        $image = request()->file('image');
+        // $extension = $image->getClientOriginalExtension();
+        $imagePath = request('image')->store('category', 'public');
+        // dd($imagePath);
+        
+        // Storage::disk('public')->put($image->getFilename().'.'.$extension,  File::get($image));
+        }
+        \App\Category::create([
+            'name' => $data['name'],
+            'image' => $imagePath ?? ''
+        ]);
+        
+        return redirect(route('category-create'));
+    }
+
+    public function update(){
+
+    }
+}
---
+++ resources/views/category/update.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="form-group row">
+    <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
+    <div class="col-md-6">
+        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+        @error('name')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+    </div>
+</div>
+
+<div class="form-group row">
+    <label for="image" class="col-md-4 col-form-label text-md-right">{{ __('Image') }}</label>
+
+    <div class="col-md-6">
+        <input id="image" type="file" class="form-control @error('image') is-invalid @enderror" name="image" value="{{ old('image') }}" required autocomplete="image" autofocus>
+
+        @error('image')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+    </div>
+</div>
+
+@endsection
