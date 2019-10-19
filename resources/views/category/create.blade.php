6d365ea28af69073ae9b18ad8cdb21548d6b0629
Niko <teddybear_happyboy@yahoo.co.id>
2019-10-19 10:22:31 +0700

    Revert "Revert "first commit""
    This reverts commit 2ab9d5fc6313e15dda3fa39c08b6ec71f4177e4b.

---
+++ resources/views/category/create.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+<form method="POST" action="{{ route('category-store') }}" enctype="multipart/form-data">
+    @csrf
+
+<div class="form-group row">
+    <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
+    <div class="col-md-6">
+        <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+        @error('name')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+    </div>
+</div>
+
+<div class="form-group row">
+    <label for="image" class="col-md-4 col-form-label text-md-right">{{ __('Image') }}</label>
+
+    <div class="col-md-6">
+        <input id="image" type="file" class="form-control @error('image') is-invalid @enderror" name="image" value="{{ old('image') }}" autocomplete="image" autofocus>
+
+        @error('image')
+            <span class="invalid-feedback" role="alert">
+                <strong>{{ $message }}</strong>
+            </span>
+        @enderror
+    </div>
+</div>
+
+<div class="form-group row mb-0">
+    <div class="col-md-6 offset-md-4">
+        <button type="submit" class="btn btn-primary">
+            {{ __('Add Category') }}
+        </button>
+    </div>
+</div>
+</form>
+
+@endsection
