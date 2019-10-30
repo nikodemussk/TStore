fcf0c8e5f669143951b4009e12fe2a61a6740c61
Niko <teddybear_happyboy@yahoo.co.id>
2019-10-30 09:39:50 +0700

    edit transaction migration

---
+++ database/migrations/2019_10_28_143412_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('quantity');
+            $table->unsignedBigInteger('clothes_id')->unsigned()->index();
+
+            $table->foreign('clothes_id')->references('id')->on('clothes')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
